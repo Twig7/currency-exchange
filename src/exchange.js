213db83e794b34cfc126532178ad4ca305d14a3e
Amber W <amoothenielson@gmail.com>
2022-01-13 22:33:53 -0800

    Create static method called exchangeRate to add API, second guess everything written.

---
+++ src/exchange.js
@@ -0,0 +1,8 @@
+export default class Exchange {
+  static exchangeRate() {
+    return fetch(`https://v6.exchangerate-api.com/v6/129122c745a5a4361f586ed0/latest/USD`)
+    .then(function(response) {
+      if()
+    })
+  })
+}
