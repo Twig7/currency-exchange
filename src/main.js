b866219910ee6d16b09186b845c7d1a9f41f2b74
Amber W <amoothenielson@gmail.com>
2022-01-14 23:01:54 -0800

    Add error message and create function exchangeShow for exchanging currencies.

--- src/main.js
+++ src/main.js
@@ -3,3 +3,12 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import Exchange from './src/exchange.js';
+
+function exchangeShow(response, usd, currency) {
+  if (response.result != 'success') {
+    $('#exchangeShow').text(`Oopsie, there was an error! ${response.message}`);
+  }
+  else if (!currency) {
+    $('#exchangeShow').text('Which currency would you like to exchange?');
+  }
+}
