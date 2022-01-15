b690cff1f668a3fb1079f91df4f582c7b6c6b98d
Amber W <amoothenielson@gmail.com>
2022-01-15 15:11:33 -0800

    push final project.

--- src/exchange.js
+++ src/exchange.js
@@ -1,10 +1,13 @@
 export default class Exchange {
-  static exchangeRate() {
-    return fetch(`https://v6.exchangerate-api.com/v6/129122c745a5a4361f586ed0/latest/USD`)
-    .then(function(response) {
-      if(!response.ok) {
+  static async exchangeRate() {
+    try {
+      const response = await fetch(`https://v6.exchangerate-api.com/v6/129122c745a5a4361f586ed0/latest/USD`);
+      if (!response.ok) {
         throw Error(response.statusText);
       }
-    })
+      return await response.json();
+    } catch (error) {
+      return error;
+    }
   }
 }
---
+++ src/index.js
@@ -0,0 +1,33 @@
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
+import Exchange from './src/exchange.js';
+
+function exchangeShow(response, usd, currency) {
+  if (response.result != 'success') {
+    $('#exchangeShow').text(`Oopsie, there was an error! ${response.message}`);
+  } else if (!currency) {
+    $('#exchangeShow').text('Which currency would you like to exchange?');
+  } else if (usd === '') {
+    $('#exchangeShow').text('How much USD are you exchanging?');
+  } else if (response.result === 'success') {
+    $('#exchangeShow').text(`${usd} in ${currency}: ${response.conversion_rates[currency]*usd}`);
+  } else {
+    $('#exchangeShow').text(`Oopsie, there was an error!`);
+  }
+}
+
+$(document).ready(function() {
+  $('#submitMoney').click(function(event) {
+    event.preventDefault();
+
+    let usd = $('#usd').val();
+    $('#usd').val("");
+    let currency = $('input:radio:checked').val();
+    Exchange.exchangeRate()
+      .then(function(response) {
+        exchangeShow(response, usd, currency);
+      });
+  });
+});
