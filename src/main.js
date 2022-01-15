3fe95e3ec2912cd0506557384c1422cd38c12398
Amber W <amoothenielson@gmail.com>
2022-01-14 23:08:02 -0800

    Write first draft of business logic, continue to second guess everything. Misplace where you can add the terminal into vs code. Find the terminal again.

--- src/main.js
+++ src/main.js
@@ -7,8 +7,13 @@ import Exchange from './src/exchange.js';
 function exchangeShow(response, usd, currency) {
   if (response.result != 'success') {
     $('#exchangeShow').text(`Oopsie, there was an error! ${response.message}`);
-  }
-  else if (!currency) {
+  }else if (!currency) {
     $('#exchangeShow').text('Which currency would you like to exchange?');
+  }else if (usd === '') {
+    $('#exchangeShow').text('How much USD are you exchanging?');
+  } else if (response.result === 'success') {
+    $('#exchangeShow').text(`${usd} in ${currency}: ${response.conversion_rate*usd}`);
+  } else {
+    $('#exchangeShow').text(`Oopsie, there was an error!`);
   }
 }
