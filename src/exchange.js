63f07a81e1d91558191c7b7c4695be6d718c9c59
Amber W <amoothenielson@gmail.com>
2022-01-14 22:56:26 -0800

    Write error message code.

--- src/exchange.js
+++ src/exchange.js
@@ -2,7 +2,9 @@ export default class Exchange {
   static exchangeRate() {
     return fetch(`https://v6.exchangerate-api.com/v6/129122c745a5a4361f586ed0/latest/USD`)
     .then(function(response) {
-      if()
+      if(!response.ok) {
+        throw Error(response.statusText);
+      }
     })
-  })
+  }
 }
